db13f3fdf34e9d4b16b9776c6c4c126a662be0c5
ASUS <ASUS@ASUS>
2024-07-08 12:39:46 +0530

    First Commit

---
+++ JavaProjects/src/sample/pgm/HelloWorld.java
@@ -0,0 +1,17 @@
+package sample.pgm;
+
+public class HelloWorld {
+	public void display()
+	{
+		System.out.println("Helloworld");
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HelloWorld obj=new HelloWorld();
+		obj.display();
+	}
+
+}
---
+++ JavaProjects/src/sample/pgm/SumofTwonumbers.java
@@ -0,0 +1,17 @@
+package sample.pgm;
+
+public class SumofTwonumbers {
+	public void sum(float a, float b)
+	{
+		float sum=a+b;
+		System.out.println("The sum of numbers:"+sum);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SumofTwonumbers obj=new SumofTwonumbers();
+		obj.sum(10.5f,20.5f);
+
+	}
+
+}
